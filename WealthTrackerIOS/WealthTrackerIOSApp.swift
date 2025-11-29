adbe5b62b56fdac142f7e76702e19f374b068dd2
Daniel Griffin <danielgriffin1@hotmail.co.uk>
2025-11-29 16:12:57 +0000

    Initial Commit

---
+++ WealthTrackerIOS/WealthTrackerIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WealthTrackerIOSApp.swift
+//  WealthTrackerIOS
+//
+//  Created by Daniel Griffin on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct WealthTrackerIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
